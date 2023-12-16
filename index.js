7d061b9c3641dbf0c027c3a87a06f53de8cac794
GustavoCaruzoGoncalves <gustavo.caruzo.goncalves@gmail.com>
2023-12-16 03:04:01 -0300

    Login e logout funcionando, correções, adições ao botão de usuário

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// index.js
+
 const express = require('express');
 const session = require('express-session');
 const db = require('./db/db');
@@ -44,8 +46,44 @@ app.get('/', (req, res) => {
   res.render('index', { nomeUsuario });  // Renderiza a página index.ejs
 });
 
+// Rota para exibir o formulário de login
+app.get('/login', (req, res) => {
+  // Recupera a variável mensagemErro da sessão
+  const mensagemErro = req.session.mensagemErro;
+
+  res.render('cadastro', { mensagemErro }); // Crie a view login.ejs para exibir o formulário de login
+});
+
+// Rota para processar o formulário de login
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const senha = req.body.senha;
+
+  // Lógica para verificar as credenciais no banco de dados
+  const sql = 'SELECT * FROM usuarios WHERE email = ? AND senha = ?';
+  db.query(sql, [email, senha], (err, results) => {
+    if (err) {
+      console.error('Erro ao verificar credenciais:', err.stack);
+      res.status(500).send('Erro ao verificar credenciais');
+      return;
+    }
+
+    if (results.length > 0) {
+      // Usuário autenticado, armazena o nome do usuário na sessão
+      req.session.nomeUsuario = results[0].nome;
+      res.redirect('/');
+    } else {
+      // Credenciais inválidas, configura a mensagem de erro na sessão
+      req.session.mensagemErro = 'Credenciais inválidas. Por favor, verifique seu email e senha.';
+      res.redirect('/cadastro');
+    }
+  });
+});
+
 app.get('/cadastro', (req, res) => {
-  res.render('cadastro'); // Assumindo que o arquivo EJS de cadastro é chamado cadastro.ejs
+  const mensagemErro = req.session.mensagemErro || '';
+  req.session.mensagemErro = '';
+  res.render('cadastro', { mensagemErro });
 });
 
 // Rota para processar o formulário de cadastro
@@ -60,6 +98,7 @@ app.post('/cadastro', (req, res) => {
   db.query(sql, [email, senha, nome], (err, result) => {
     if (err) {
       console.error('Erro ao cadastrar usuário:', err.stack);
+      req.session.mensagemErro = 'Erro ao cadastrar usuário. Por favor, tente novamente.';
       res.status(500).send('Erro ao cadastrar usuário');
       return;
     }
@@ -70,6 +109,13 @@ app.post('/cadastro', (req, res) => {
     // Redireciona para o index.ejs com o nome do usuário
     res.redirect('/');  // Redireciona para a rota '/' após o cadastro
   });
+});
+
+// Rota para efetuar logout
+app.get('/logout', (req, res) => {
+  // Limpa a variável nomeUsuario da sessão
+  req.session.nomeUsuario = null;
+  res.redirect('/');
 });
 
 app.listen(port, () => {
